2f07f9e61cbf611669f756d6fd2b43a2d76694cd
AmkaTsendjargal <47435015+AmkaTsendjargal@users.noreply.github.com>
2019-02-24 15:38:19 -0700

    JavaDoc'd

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 public class ComputerPlayer{
 //This is the AI computer controlled player for the Monopoly game.
+
+//initializes player name, balance and owned properties
 private String name;
 private int balance = 1500;
 private ArrayList<Square> propertiesOwned = new ArrayList<Square>();
     public ComputerPlayer(String aName){
-    this.name = aName;   
+    this.name = aName;
     }
-    
+
 
     /**
-     * @param name the name to set
+     * This method takes some name and sets it as a certain player's name
+     * @param name The name of the player
      */
     public void setName(String aName) {
         name = aName;
@@ -19,7 +22,8 @@ private ArrayList<Square> propertiesOwned = new ArrayList<Square>();
 
 
     /**
-     * @return the getname
+     * This method returns a specific player's given name
+     * @return name The player's name
      */
     public String getName() {
         return name;
@@ -27,29 +31,35 @@ private ArrayList<Square> propertiesOwned = new ArrayList<Square>();
 
 
     /**
-     * @return the balance
+     * This method returns a certain player's balance
+     * @return balance The player's balance
      */
     public int getBalance() {
         return balance;
     }
 
     /**
-     * @param balance the balance to set
+     * This method takes a certain integer and sets it as a specific player's balance
+     * @param balance The number set to a player's balance
      */
     public void setBalance(int balance) {
         this.balance = balance;
     }
 
     /**
+     * This method returns the list of properties owned by a player
      * @return the propertiesOwned
      */
     public ArrayList<Square> getPropertiesOwned() {
         return propertiesOwned;
     }
 
+    /**
+     * @param i 
+     */
     public void move(int i) {
-        
+
     }
-    
-    
+
+
 }
