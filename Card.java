89990219d839084a856062e381302fde13c81d32
Simon Vincent <47257674+SmonVincent@users.noreply.github.com>
2019-03-12 12:36:04 -0600

    March 12/19 build.

---
+++ Card.java
@@ -0,0 +1,16 @@
+public class Card {
+/**
+ * This Class provides the skeleto for the creation of chance cards.
+ */
+
+public String name;
+public String action;
+public int value;
+
+    
+    Card(String aName, String anAction, int aValue) {
+        this.name = aName;
+        this.action = anAction;
+        this.value = aValue;
+    }
+}
