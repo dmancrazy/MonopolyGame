b9b1ebe23323236fbb3179199c5805d8dbf2bbf3
dmancrazy <47063575+dmancrazy@users.noreply.github.com>
2019-04-12 03:52:03 -0600

    Delete TextBasedGame.java

--- TextBasedGame.java
+++
@@ -1,8 +0,0 @@
-public class TextBasedGame {
-
-	public static void main(String[] args) {
-    	MonopolyGame monopolyGame = new MonopolyGame();
-    	monopolyGame.setup();
-    	monopolyGame.play();
-	}
-}
